ef82bee1f4b6bd5181e536ad3ffcf38d7e98e21d
Katilmami <93917818+Katilmami@users.noreply.github.com>
2023-09-09 15:01:47 +0300

    Add files via upload

---
+++ mamiscripttttt.lua
@@ -0,0 +1,399 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+
+local Window = OrionLib:MakeWindow({Name = "Mami Script V6", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Mami = Window:MakeTab({
+	Name = "Mami",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami:AddSection({
+	Name = "Mami"
+})
+
+Mami:AddButton({
+	Name = "Kanal Katil Mami",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Abone Olmayanı Sikerim",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Yunus Kraldır Gerisi Yalandır",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+Mami:AddButton({
+	Name = "Mami Script Sunar",
+	Callback = function()
+      		print("Mamiaaaaa")
+  	end    
+})
+
+local Mami2 = Window:MakeTab({
+	Name = "Scriptler",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami2:AddSection({
+	Name = "Scriptler"
+})
+
+Mami2:AddButton({
+	Name = "Uçma",
+	Callback = function()
+      		loadstring(game:HttpGet('https://pastebinp.com/raw/YSL3xKYU'))()
+  	end    
+})
+
+Mami2:AddButton({
+	Name = "Klavye",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+  	end    
+})
+
+
+Mami2:AddButton({
+	Name = "Fps Boost",
+	Callback = function()
+      		loadstring(game:HttpGet('https://pastebinp.com/raw/zJztwUg1'))()
+              setfpscap(10000)
+  	end    
+})
+Mami2:AddButton({
+	Name = "Fps Boost V2",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebinp.com/raw/GSWA1L6y"))()
+              setfpscap(10000)
+  	end    
+})
+Mami2:AddButton({
+	Name = "Bedava Gamepass",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebinp.com/raw/Wqxzdi1q"))()
+  	end    
+})
+Mami2:AddButton({
+	Name = "Rtx",
+	Callback = function()
+      		getgenv().RTX_Name = "Midday lite" 
+              loadstring(game:HttpGet(('https://pastefy.app/xXkUxA0P/raw'),true))()
+  	end    
+})
+Mami2:AddButton({
+	Name = "Süper Adam",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 120
+              game.Players.LocalPlayer.Character.Humanoid.JumpPower = 120
+  	end    
+})
+Mami2:AddButton({
+	Name = "Admin Komutları",
+	Callback = function()
+      		loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+  	end    
+})
+
+
+local Mami3 = Window:MakeTab({
+	Name = "Oyuncu",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami3:AddSection({
+	Name = "Oyuncu"
+})
+
+
+Mami3:AddButton({
+	Name = "Hız",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 500
+  	end    
+})
+Mami3:AddButton({
+	Name = "Zıplama",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.Humanoid.JumpPower = 350
+  	end    
+})
+Mami3:AddButton({
+	Name = "Sıfırla",
+	Callback = function()
+      		game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
+              game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+  	end    
+})
+
+local Mami4 = Window:MakeTab({
+	Name = "Diğer",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami4:AddSection({
+	Name = "Diğer"
+})
+Mami4:AddButton({
+	Name = "Anti Ban (Hileyi Tamamen Gizler)",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebinp.com/raw/Wqxzdi1q"))()
+  	end    
+})
+Mami4:AddButton({
+	Name = "Sohbet Sahtekarlığı",
+	Callback = function()
+		      loadstring(game:HttpGet(('https://pastebinp.com/raw/djBfk8Li'),true))()
+  	end    
+})
+Mami4:AddButton({
+	Name = "Bypass Uçma",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/BypassedFly.lua"))() 
+		      Fly(true)
+  	end    
+})
+
+
+local Mami5 = Window:MakeTab({
+	Name = "Eşyalar",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami5:AddSection({
+	Name = "Eşyalar"
+})
+
+Mami5:AddButton({
+	Name = "Kılıçlar",
+	Callback = function()
+		      loadstring(game:HttpGet("https://pastebinp.com/raw/AZVi2tuK"))()
+  	end    
+})
+
+Mami5:AddButton({
+	Name = "Oyun Düzenleme İtemi",
+	Callback = function()
+		      loadstring(game:GetObjects("rbxassetid://6695644299")[1].Source)()
+  	end    
+})
+Mami5:AddButton({
+	Name = "Kurucu İtemi",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/Katilmami/Mamiscriptt1019199/main/mami.lua"))()
+  	end    
+})
+
+Mami5:AddButton({
+	Name = "1000 Tane İtem",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/PlanetHubX/Lucky-Blocks/main/source", true))()
+  	end    
+})
+
+
+
+
+local Mami6 = Window:MakeTab({
+	Name = "Admin",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami6:AddSection({
+	Name = "Admin"
+})
+
+
+Mami6:AddButton({
+	Name = "Kurucu İtemi",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/Katilmami/Mamiscriptt1019199/main/mami.lua"))()
+  	end    
+})
+Mami6:AddButton({
+	Name = "Admin Komutları",
+	Callback = function()
+      		loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+  	end    
+})
+Mami6:AddButton({
+	Name = "Ban Kick Paneli",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/harked", true))()
+  	end    
+})
+
+
+local Mami7 = Window:MakeTab({
+	Name = "Oyunlar",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami7:AddSection({
+	Name = "Oyunlar"
+})
+
+
+
+Mami7:AddButton({
+	Name = "Bedava Brookhaven Premium",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMael7/AvatarUnlocker/main/IceHub"))()
+  	end    
+})
+Mami7:AddButton({
+	Name = "Katil VS Şerif Key NiceMurderers4211Roblox",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/Duels214/main/Sheriffs1"))()
+  	end    
+})
+Mami7:AddButton({
+	Name = "Lumber Tycoon 2",
+	Callback = function()
+		      loadstring(game:HttpGet("https://raw.githubusercontent.com/frencaliber/LuaWareLoader.lw/main/luawareloader.wtf",true))()
+  	end    
+})
+Mami7:AddButton({
+	Name = "Doors Script",
+	Callback = function()
+		      loadstring(game:HttpGetAsync("https://pastebinp.com/raw/R8QMbhzv"))()
+  	end    
+})
+
+Mami7:AddButton({
+	Name = "Blox Fruit",
+	Callback = function()
+		      loadstring(game:HttpGet(('https://raw.githubusercontent.com/Tulam2000/FTS-HUB/main/FTS%20x%20Hub')))()
+  	end    
+})
+Mami7:AddButton({
+	Name = "Build A Boat",
+	Callback = function()
+		      loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'))()
+  	end    
+})
+
+
+local Mami8 = Window:MakeTab({
+	Name = "Troll",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Mami8:AddSection({
+	Name = "Troll"
+})
+
+Mami8:AddButton({
+	Name = "Tıkladığın Kişiyi Fırlat",
+	Callback = function()
+		      loadstring(game:HttpGet(('https://raw.githubusercontent.com/0Ben1/fe/main/obf_5wpM7bBcOPspmX7lQ3m75SrYNWqxZ858ai3tJdEAId6jSI05IOUB224FQ0VSAswH.lua.txt'),true))()
+  	end    
+})
+Mami8:AddButton({
+	Name = "İsmi Girileni Fırlat",
+	Callback = function()
+		      loadstring(game:HttpGet('https://pastebinp.com/raw/fj5VwQtC'))()
+  	end    
+})
+
+Mami8:AddButton({
+	Name = "Troll Script",
+	Callback = function()
+		      loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/GhostHub'))()
+  	end    
+})
+
+Mami8:AddButton({
+	Name = "Admin Troll",
+	Callback = function()
+		      loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+  	end    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
